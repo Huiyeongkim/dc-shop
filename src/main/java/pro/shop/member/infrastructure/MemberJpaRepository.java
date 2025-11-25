f0e548a127b0d1c66339ac0dfe7da859e66fa9d3
Huiyeongkim <huiyeong9619@naver.com>
2025-11-25 10:32:15 +0900

    refactor: memberservice msa

--- src/main/java/pro/shop/member/infrastructure/MemberJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package pro.shop.member.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pro.shop.member.domain.Member;
-
-import java.util.UUID;
-
-public interface MemberJpaRepository extends JpaRepository<Member, UUID> {
-}
